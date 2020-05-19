fff99f1b78f13310172a92d3652f95e2865cd206
riddhi0806 <33753283+riddhi0806@users.noreply.github.com>
2020-05-19 12:22:14 +0530

    Update urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -12,4 +12,4 @@ urlpatterns = [
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
-]v
+]
